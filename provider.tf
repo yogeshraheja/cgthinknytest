43a1cf846ffb7d1a35b6f19b7605f240957c0e2a
yogeshraheja <yogesh.10d@gmail.com>
2022-07-14 17:42:53 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,11 +2,10 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "4.5.0"
     }
   }
 }
 
 provider "aws" {
-  # Configuration options
+  region = "us-east-1"
 }
